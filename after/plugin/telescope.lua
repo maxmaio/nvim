90451b4f9d3db1193f946275949808feef7ddd41
maxmaio <maxmaio8@gmail.com>
2024-02-22 15:19:18 -0800

    dont search .env/

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,10 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>st', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>st', function()
+  builtin.grep_string({
+    additional_args = function(opts)
+      return {"--exclude-dir", ".env"}
+    end
+  })
+end, {})
